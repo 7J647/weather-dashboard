f02be4c0743f8b4934df779a9ec7e14270c7e836
7J647 <007J647@gmail.com>
2020-09-14 00:01:04 -0400

    tied in the city input with the URL builder and have the object returning response as expected

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// var searchSection = $("#searchSection"); 
+// $("$searchCity").attr("style", "float:left");
+$("#searchButton").attr("style", "background-color: #007bff; color: white; height:30px; width:70px");
+
+// var searchButton = $("<button>").attr("style", "background-color: #007bff; float:left; height:30px; width:30px");
+// searchSection.append(searchButton);
+
+// var searchIcon = $("<img>");
+//     searchIcon.attr("src", "https://icons-for-free.com/iconfiles/png/512/magnifying+glass+search+zoom+icon-1320196461375500720.png");
+//     searchIcon.attr("alt", "search");
+//     searchIcon.attr("style", "display:block; margin-left:auto; margin-right:auto");
+//     searchButton.append(searchIcon);
+
+ 
+
+
+
+
+
